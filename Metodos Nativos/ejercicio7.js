75b5f38eab9057d184ea6c0b22b28a5c08f8125d
Adrian Mateos <adrian@adrian>
2020-10-13 12:24:19 +0200

    [MOD] Ejercicio 7 avanzado

--- Metodos Nativos/ejercicio7.js
+++ Metodos Nativos/ejercicio7.js
@@ -1,7 +1,19 @@
 function calvaland(){
     var notas = prompt("Mete notas separadas por comas: ");
-    var arrayNotas = notas.split();
-    var media = {
-        
-    }
+    var arrayNotas = notas.split(",");
+    
+    arrayNotas = arrayNotas.map(Barras => Number(Barras));
+    var media = arrayNotas.reduce((n1, n2) => (n1 + n2))/arrayNotas.length;
+    
+    console.log(media.toFixed(1));
 }
+
+
+ function pajascu(numero){
+    if(!isNaN(numero)){
+        return numero * numero * numero;
+    }else if(!isNaN(numero)){
+        alert("Eso no es un numero entero");
+        return false;
+    }
+ }
